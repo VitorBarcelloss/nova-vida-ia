708a8eb22d63e7903b771fedb117b69f122c89fc
vitor <vitor@caletagaming.com>
2024-10-29 21:58:41 -0300

    feat: initial setup

---
+++ nova_vida_ia/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nova_vida_ia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nova_vida_ia.settings')
+
+application = get_wsgi_application()
