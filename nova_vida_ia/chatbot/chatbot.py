421f56a2f7d32f79793aced83765337fda469eaa
vitor <vitor@caletagaming.com>
2024-10-31 19:20:52 -0300

    feat: added memory and langchain to the chatbot implementation

--- nova_vida_ia/chatbot/chatbot.py
+++ nova_vida_ia/chatbot/chatbot.py
@@ -1,8 +1,11 @@
 import json
+from uuid import uuid4
 
-import chromadb
 from django.http import JsonResponse
-from groq import Groq
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_groq import ChatGroq
+from langgraph.constants import START
+from langgraph.graph import MessagesState, StateGraph
 
 from nova_vida_ia import settings
 from nova_vida_ia.chatbot.memory import ChromaDB
@@ -10,47 +13,47 @@ from nova_vida_ia.chatbot.memory import ChromaDB
 
 class Chatbot:
     def __init__(self, request):
-        self.client = Groq(api_key=settings.API_KEY)
         self.chatbot_model = settings.CHATBOT_MODEL
+        self.client = ChatGroq(api_key=settings.API_KEY, model_name=self.chatbot_model, temperature=0.3)
         self.request_data = json.loads(request.body.decode())
         self.vector_db = ChromaDB()
+        self.graph = StateGraph(state_schema=MessagesState)
 
         with open('./nova_vida_ia/chatbot/prompts/prompt_template.txt', 'r') as file:
             self.system_prompt = file.read()
 
-
     def message(self):
+        event_chats = []
         user_msg = self.request_data['user_msg']
-        messages = self.get_messages(user_msg)
+        user_preferences = self.vector_db.get_user_preferences()
+        previous_data = self.vector_db.get_previous_data()
+        graph = self.graph
 
-        chat_completion = self.client.chat.completions.create(
-            messages=messages,
-            model=self.chatbot_model,
-            temperature=0.6,
-            max_tokens=2048,
-            top_p=0.5,
-            stop=None,
-            stream=False
-        )
+        graph.add_edge(START, "client")
+        graph.add_node("client", self.invoke_client)
+        app = graph.compile()
 
-        assistant_response = chat_completion.choices[0].message.content
-        self.vector_db.create_memory(assistant_response, "assistant")
-        self.vector_db.create_memory(user_msg, "user")
+        config = {"configurable": {"thread_id": uuid4()}}
+
+        template = (f"Seu Template: {self.system_prompt}\n"
+                    f"Memórias: {' '.join(previous_data)}\n"
+                    f"Preferências do usuário: {user_preferences}")
+
+        system_message = SystemMessage(content=template)
+        input_message = HumanMessage(content=user_msg)
+        for event in app.stream({"messages": [system_message, input_message]}, config, stream_mode="values"):
+            event_chats.append(event["messages"][-1])
+
+        assistant_response = event_chats[-1].content
+
+        self.vector_db.store_user_preferences(user_msg, self.client, self.chatbot_model)
+        self.vector_db.create_memory(assistant_response, user_msg)
 
         return JsonResponse({
-                "response_message":chat_completion.choices[0].message.content
-            })
+            "response_message": assistant_response
+        })
 
-    def get_messages(self, user_msg):
-        previous_message = self.vector_db.get_previous_messages()
-        messages = [
-            {
-                "role": "user",
-                "content": user_msg,
-            },
-            {
-                "role": "system",
-                "content": self.system_prompt,
-            }]
+    def invoke_client(self, state: MessagesState):
+        response = self.client.invoke(state["messages"])
 
-        return previous_message + messages
+        return {"messages": response}
--- nova_vida_ia/chatbot/memory.py
+++ nova_vida_ia/chatbot/memory.py
@@ -2,7 +2,6 @@ import json
 from uuid import uuid4
 
 import chromadb
-from chromadb import Settings
 
 
 class ChromaDB:
@@ -10,34 +9,63 @@ class ChromaDB:
         self.vector_db = chromadb.HttpClient(host='localhost', port=8001)
         self.collection = self.vector_db.get_or_create_collection("chatbot_memory")
 
-    def get_previous_messages(self):
-        previous_messages = []
+    def get_user_preferences(self):
+        preferences = []
         memory = self.collection.get()
 
         for doc in memory.get('documents'):
             json_doc = json.loads(doc)
+            if json_doc['metadata']['role'] == 'user_preferences':
+                preferences.append(json_doc['content'])
 
-            previous_messages.append({
-                "role": json_doc['metadata']['role'],
-                "content": json_doc['content']
-            })
+        return preferences
 
-        return previous_messages
+    def get_previous_data(self):
+        previous_data = []
+        memory = self.collection.get()
 
-    def create_memory(self, content, role):
-        embedding = self.create_embedding(content)
-        documents = {
-            "content": content,
-            "metadata": {"role": role}
-        }
-        json_documents = json.dumps(documents)
+        for doc in memory.get('documents'):
+            json_doc = json.loads(doc)
+            if json_doc['metadata']['role'] != 'user_preferences':
+                previous_data.append(json_doc['content'])
+
+        return previous_data
+
+    def store_user_preferences(self, user_message, client, chatbot_model):
+        preferences_prompt = (f"Detecte as preferencias do usuario nesta mensagem e somente liste elas, "
+                              f"se não houver nenhuma preferencia, não retorne nada: {user_message}")
+
+        try:
+            response = client.invoke(preferences_prompt)
+
+            if response.content != 'Nenhuma preferência detectada.':
+                documents = json.dumps({
+                    "content": response.content.replace('\n', ' '),
+                    "metadata": {"role": "user_preferences"}
+                })
+
+                self.collection.add(
+                    ids=[str(uuid4())],
+                    documents=[documents]
+                )
+        except Exception as e:
+            print(e)
+
+    def create_memory(self, ai_message, user_message):
+        ai_message = json.dumps({
+            "content": ai_message,
+            "metadata": {"role": "assistant"}
+        })
+
+        user_message = json.dumps({
+            "content": user_message,
+            "metadata": {"role": "user_msg"}
+        })
 
         self.collection.add(
-            ids=[str(uuid4())],
-            documents=[json_documents],
-            embeddings=[embedding]
+            ids=[str(uuid4()), str(uuid4())],
+            documents=[ai_message, user_message]
         )
 
-    #TODO criar embedding
-    def create_embedding(self, content):
-        return [0.1] * 768
+
+
